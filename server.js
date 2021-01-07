aa1760966d74b30f9664f42010d7e9504e4a3eda
Yasamin Yaldaei <yasaminyaldaei@gmail.com>
2021-01-07 13:54:16 +0000

    modify config

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
 const runner            = require('./test-runner');
 
+const mongoose    = require('mongoose')
+
+
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -27,8 +30,24 @@ app.route('/')
 //For FCC testing purposes
 fccTestingRoutes(app);
 
+mongoose.connect(process.env.DB, 
+                { useNewUrlParser: true,
+                  useUnifiedTopology: true })
+
+const connection = mongoose.connection
+connection.on('error', console.error.bind(console, "connection error"))
+connection.once('open', () => {
+  console.log("MongoDB Database connection established successfully")
+})
+
+const Schema = mongoose.Schema
+const bookSchema = new Schema({
+  title: { type: String, required: true}
+})
+const Book = mongoose.model("Book", bookSchema)
+
 //Routing for API 
-apiRoutes(app);  
+apiRoutes(app, Book);  
     
 //404 Not Found Middleware
 app.use(function(req, res, next) {
@@ -39,7 +58,7 @@ app.use(function(req, res, next) {
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Listening on port " + process.env.PORT);
+  console.log("Listening on port " + process.env.PORT || 3000);
   if(process.env.NODE_ENV==='test') {
     console.log('Running Tests...');
     setTimeout(function () {
